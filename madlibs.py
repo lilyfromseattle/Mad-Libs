04909b9346eaf286784e87d25445b23095098468
lilyfromseattle <lilyfpace@gmail.com>
2013-10-14 17:30:39 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+print "Welcome to the random story generator!"
+adj1 = raw_input("Enter an adjective:")
+noun1 = raw_input("Enter a noun:")
+noun2 = raw_input("Enter a noun:")
+place1 = raw_input("Enter a place:")
+
+
+print "Once upon a time there was a " + adj1 +" "+ noun1 + "." + " The " + noun1 + " lived in a " + noun2 + " in the middle of " + place1 + "." 
+
+#!/usr/bin/env python
+
